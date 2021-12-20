0639e8a89991f3c04b8b79b50bce99c84d2c6e82
Kaj Koole <kajkoole@hva.nl>
2021-12-20 19:14:52 +0100

    Add SendMailTest.py

---
+++ python/SendMailTest.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+'''
+SendMailTest.py: Python script for Wk6 CTI assignment 2 Python output.
+'''
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+SMTP_SERVER = 'smtp.gmail.com'
+SMTP_SSLPORT = 465
+SMTP_USERNAME = 'someone@example.com'
+SMTP_PASSWORD = 'supersecret'
+
+FROM_ADDRESS = 'someone@example.com'
+
+
+def SendGmail(toAddress, subject, msg):
+    msg = MIMEMultipart()
+    msg['From'] = FROM_ADDRESS
+    msg['To'] = toAddress
+    msg['Subject'] = subject
+
+    body = 'This is a test sending email through python'
+    msg.attach(MIMEText(body, 'plain'))
+
+    server = smtplib.SMTP_SSL(SMTP_SERVER, SMTP_SSLPORT)
+    server.login(SMTP_USERNAME, SMTP_PASSWORD)
+    text = msg.as_string()
+    server.sendmail(FROM_ADDRESS, toAddress, text)
+
+
+def main():
+    toAddress = 'someone@example.com'
+    subject = "Test"
+    msg = "Test\nRegards"
+
+    SendGmail(toAddress, subject, msg)
+    
+
+if __name__ == '__main__':
+    main()
